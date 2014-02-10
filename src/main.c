3585eb65a0f979ec269984bfe7906e89e4a963d0
Milo Price <milo.price@gmail.com>
2014-02-10 11:50:17 -0800

    Step 2 of tutorial complete
    http://ninedof.wordpress.com/2013/12/18/pebble-sdk-2-0-tutorial-2-telling-the-time/

--- src/main.c
+++ src/main.c
@@ -2,23 +2,52 @@
 
 Window *window;
 TextLayer *text_layer;
+char buffer[] = "00:00";
+InverterLayer *inv_layer;	//Inverter layer
+
+void tick_handler(struct tm *tick_time, TimeUnits units_changed){
+	//Format the buffer string using tick_time as the time source
+	strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+	
+	//Change the TextLayer text to show the new time
+	text_layer_set_text(text_layer, buffer);
+}
 
 void window_load(Window *window)
 {
-	text_layer = text_layer_create(GRect(0, 0, 144, 168));
+	text_layer = text_layer_create(GRect(0, 53, 132, 168));
 	text_layer_set_background_color(text_layer, GColorClear);
 	text_layer_set_text_color(text_layer, GColorBlack);
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
 	
 	layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
 	
 	//Arbitrary text:
 	text_layer_set_text(text_layer, "Radscorpion sighted");	
+	
+	//Inverter layer
+	inv_layer = inverter_layer_create(GRect(0,50,144,62));
+	layer_add_child(window_get_root_layer(window), (Layer*) inv_layer);
+	
+	//Get time structure so face doesn't start blank
+	struct tm *t;
+	time_t temp;
+	temp = time(NULL);
+	t = localtime(&temp);
+	
+	//Manually call tick handler when window is loading
+	tick_handler(t, MINUTE_UNIT);
 }
 
 void window_unload(Window *window)
 {
+	text_layer_destroy(text_layer);
 	
+	inverter_layer_destroy(inv_layer);
 }
+
+
 	
 void handle_init(void) {
 	window = window_create();
@@ -27,13 +56,14 @@ void handle_init(void) {
 		.unload = window_unload,
 	});
 	
-	
 	window_stack_push(window, true);
+	
+	tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
 }
 
 void handle_deinit(void) {
-	text_layer_destroy(text_layer);
 	window_destroy(window);
+	tick_timer_service_unsubscribe();
 }
 
 int main(void) {
